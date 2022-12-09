7d5e53d97aae0de6503784ee0727ef3a3e9fb73d
Nick-Altland <naltland@uvm.edu>
2022-12-08 21:31:30 -0500

    Final commit of programs

--- programs/autoWateringProgram.py
+++ programs/autoWateringProgram.py
@@ -1,13 +1,24 @@
+#Import libraries
 import mysql.connector
 import json
 import smbus
 import datetime
 import time
 import RPi.GPIO as GPIO
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
 
 # Constants
-MOIST_LEVEL = 40 # What is the acceptable minimum moisture level
+MOIST_LEVEL = 4 # How many seconds the solenoid will be open
 CHECK_RATE = 1  # How often do we want the program to run, in minutes
+MAX_MOIST = 47000  # Max moisture for the sensor
+MIN_MOIST = 18000   # Min moisture for the sensor
+MOISTURE = 20  # What percent moisture we want
+
+ACTUAL_MAX_MOIST = MAX_MOIST - MIN_MOIST #After sensors is calebrated
 
 # Define SMBus (System Management Bus) port and address for
 # I2C sensor communcation
@@ -15,14 +26,39 @@ CHECK_RATE = 1  # How often do we want the program to run, in minutes
 BUS = smbus.SMBus(1)
 ADDRESS = 0x48
 
+# create the spi bus
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+
+# create the cs (chip select)
+cs = digitalio.DigitalInOut(board.D5)
+
+# create the mcp object
+mcp = MCP.MCP3008(spi, cs)
+
+# create an analog input channel on pin 0
+chan = AnalogIn(mcp, MCP.P0)
+
+# This is for initial state
+from ISStreamer.Streamer import Streamer
+
+# Streamers to import data from the inital states.
+# Structure borrowed from lab 08, but modified
+# Streamer for the moisture levels
+moist_streamer= Streamer(bucket_name="moisture", bucket_key="XCLS9WHG5QQH", access_key="ist_X2Iuw5yA30z4RVis-xsDv096lT0LkzaC")
+# Streamer for the Watering
+# watering_streamer= Streamer(bucket_name="watering",bucket_key="QS5TRLT67KNP", access_key="ist_X2Iuw5yA30z4RVis-xsDv096lT0LkzaC")
+# Streamer for the temp levels
+temp_streamer= Streamer(bucket_name="temp_charts", bucket_key="SJDT8SAWR34E", access_key="ist_X2Iuw5yA30z4RVis-xsDv096lT0LkzaC")
+
 # Set up GPIO pins for the solenoid and moisture sensor
 GPIO.setmode(GPIO.BCM) #use BCM because we are dealing with incoming voltage
 GPIO.setup(21, GPIO.OUT) # Solenoid out
+GPIO.setup(26, GPIO.IN)  # moisture sensor in
 
 # Table Header for display output
 # Taken from lab07, edited to fit
 def show_table_header():
-    print("DATE\t   TIME \t    Temp \t Moisture")
+    print("DATE\t   TIME \t    Temp \t Moist %")
     print("------------------------------------------------")
 
 # Read the sensor data from the temperature sensor, return a value in Celsius
@@ -34,9 +70,18 @@ def get_temperature():
     rvalue = (((rvalue2 * 256) + rvalue1) >> 4 ) * .0625
     return rvalue
 
-def moisture_sensor(level):
-    # Checks the value from the moisture sensor. If it is lower then then the set level,
-    return level
+def moisture_sensor():
+    # Checks the value from the moisture sensor. If it is lower then the desired number, open up valve to water then close it
+    moist_level = (1-(chan.value-MIN_MOIST)/ACTUAL_MAX_MOIST)*100
+
+    if moist_level < MOISTURE:
+        GPIO.output(21,True)
+        time.sleep(0.25)
+        GPIO.output(21,False)
+    # print(chan.value)
+    # print(moist_level)
+
+    return moist_level
 
 # Take data from sensor and monitor any changes
 def temp_data():
@@ -47,9 +92,90 @@ def temp_data():
     # Return the temp
     return temp
 
+def commit_temp(database, cursor, now, temp):
+    # SQL insert statment for temp
+    insert_temp_sql = "INSERT INTO `temp_data` (`timestamp`, `temp`) VALUES (%s,%s);"
+
+    # Insert temp data into database
+    data = (now, temp)
+    cursor.execute(insert_temp_sql,data)
+
+    # Commit insert to database
+    database.commit()
+
+    # Streamer to send data to graph
+    temp_streamer.log("temp_charts", temp)
+    temp_streamer.flush()
+    return
+
+def commit_moisture(database, cursor, now, moist_level):
+    # SQL insert statment for moist
+    insert_moist_sql = "INSERT INTO `moisture_data` (`timestamp`, `moisture_level`) VALUES (%s,%s);"
+
+    # Insert moisture data into database
+    data = (now, moist_level)
+    cursor.execute(insert_moist_sql, data)
+
+    # Commit insert to database
+    database.commit()
+
+    # Streamer to send data to graph
+    moist_streamer.log("moisture", moist_level)
+    moist_streamer.flush()
+    return
+
+def commit_needs_watering(database, cursor, now):
+    # SQL insert statment for if the plant has been watered
+    water_insert_sql = "INSERT INTO `watering_data` (`timestamp`, `watered`) VALUES (%s, %s);"
+
+    # Insert moisture data into database
+    data = (now, 1)
+    cursor.execute(water_insert_sql, data)
+
+    # Commit insert to database
+    database.commit()
+
+    # Streamer to send data to graph
+    # watering_streamer.log("watering", 1)
+    # watering_streamer.flush()
+
+    return
+
+# Function for setting up connection to database
+# Code structure from lab 07
+def database_info(now, temp, moist_level):
+    # Load database user credentials from JSON
+    credentials = json.load(open("credentials.json", "r"))
+
+    # Connect to database
+    database = mysql.connector.connect(
+        host=credentials["host"],
+        user=credentials["user"],
+        passwd=credentials["password"],
+        database=credentials["database"]
+    )
+
+    # Create cursor object that executes database commands
+    cursor = database.cursor()
+
+    # Send temp data to database
+    commit_temp(database, cursor, now, temp)
+
+    # Send moisture data to database
+    commit_moisture(database, cursor, now, moist_level)
+
+    # if moist_level < MOIST_LEVEL:
+        #commit_needs_watering(database, cursor, now)
+
+    # Close cursor and database
+    cursor.close()
+    database.close()
+
+    return
+
 # Display data to the screen, then send it to the database. Main loop
 # Starter code borrowed from lab07, but heavily modified
-def display_data():
+def store_display_data():
     # Get time
     now = datetime.datetime.now()
 
@@ -58,16 +184,17 @@ def display_data():
 
     # Call the moisture sensor, to check to see if the plant needs to be watered
     # Record the moisture level for output
-    moist_level = moisture_sensor(41)
+    moist_level = moisture_sensor()
 
     # Display data record to screen
     # Display data written by Professor Eddy, but modified by Nick
     timestamp = now.strftime("%m/%d/%Y %H:%M")
     outstring = str(timestamp) + "\t" + str(format(temp, '10.4f')) + "F" \
-    + str(format(moist_level, '10.0f'))
+    +"\t" + str(format(moist_level, '7.0f')) + "%" + "\n"
     print(outstring.rstrip())
 
-    return temp, moist_level
+    #Return the temp and moisture level
+    return temp, moist_level, now
 
 # Show table header
 show_table_header()
@@ -85,26 +212,22 @@ try:
 
     while True:
         # Call the display_data function to get the moisture level and new temp
-        newTemp, needWatering = display_data()
+        newTemp, moist_level, now = store_display_data()
         averageArray.append(newTemp) # Add new temp to the array of temps
 
-        # Check to see if the soil is dry enough to water
-        if needWatering > MOIST_LEVEL:
-            GPIO.output(21,True)
-            time.sleep(15)
-            GPIO.output(21,False)
-            # Add one to the times the plant was watered
-            timesWatered += 1
+        # Call Database to store information
+        database_info(now, newTemp, moist_level)
 
         # Orient the cycle to now start every 15 min, on the hour (00, 15, 30, 45, etc...)
         minsToSleep = CHECK_RATE - datetime.datetime.now().minute % CHECK_RATE
-        time.sleep(minsToSleep * 60)
+        time.sleep(1) #time.sleep(minsToSleep * 60)
+
 
 # Cleanup GPIO pins on program termination
 finally:
     # Call function to print data to screen
     averageTemp = (sum(averageArray)) / len(averageArray)
-    print("The average temp was: " + str(averageTemp) + " and the plant was watered " + str(timesWatered) + " times")
+    print("The average temp was: " + str(format(averageTemp, "10.4f")))
     GPIO.cleanup()
 
 
--- programs/test.py
+++ programs/test.py
@@ -4,22 +4,45 @@ import smbus
 import datetime
 import time
 import RPi.GPIO as GPIO
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+
 
 # Define SMBus (System Management Bus) port and address for
 # I2C sensor communcation
 # ALL CODE FOR TEMP SENSOR WRITTEN BY PROFESOR EDDY, PULLED FROM LAB 07-08
 BUS = smbus.SMBus(1)
 ADDRESS = 0x48
+MAX_MOIST = 48000
+MIN_MOIST = 17000
+MOISTURE = 20
+ACTUAL_MAX_MOIST = MAX_MOIST - MIN_MOIST
+
+# create the spi bus
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
+
+# create the cs (chip select)
+cs = digitalio.DigitalInOut(board.D5)
+
+# create the mcp object
+mcp = MCP.MCP3008(spi, cs)
+
+# create an analog input channel on pin 0
+chan = AnalogIn(mcp, MCP.P0)
 
 # Set up GPIO pins for the solenoid and moisture sensor
 GPIO.setmode(GPIO.BCM) #use BCM because we are dealing with incoming voltage
 GPIO.setup(21, GPIO.OUT) # Solenoid out
+GPIO.setup(26,GPIO.IN) #Moisture in
 
 # Table Header for display output
 # Taken from lab07, edited to fit
 def show_table_header():
-    print("DATE\t   TIME \t    High \t Low \t Average   Moisture")
-    print("-------------------------------------------------------------------")
+    print("DATE\t   TIME \t    High \t Low \t Average \t Moisture")
+    print("-------------------------------------------------------------------------")
 
 # Read the sensor data from the temperature sensor, return a value in Celsius
 # Taken from lab07, no changes made
@@ -30,14 +53,19 @@ def get_temperature():
     rvalue = (((rvalue2 * 256) + rvalue1) >> 4 ) * .0625
     return rvalue
 
-def moisture_sensor(level):
+def moisture_sensor():
     # Checks the value from the moisture sensor. If it is lower then then the set level,
     # open up valve to water then close it
-    if level % 2 == 0:
+    moist_level = (1-(chan.value-MIN_MOIST)/ACTUAL_MAX_MOIST)*100
+
+    if moist_level < MOISTURE:
         GPIO.output(21,True)
-        time.sleep(2)
+        time.sleep(0.25)
         GPIO.output(21,False)
-    return level
+
+    # print(moist_level)
+
+    return moist_level
 
 # Take data from sensor and monitor any changes
 def temp_data(high, low, average):
@@ -75,14 +103,14 @@ def display_data(highTemp, lowTemp, averageTemp):
 
     # Call the moisture sensor, to check to see if the plant needs to be watered
     # Record the moisture level for output
-    moist_level = moisture_sensor(2)
+    moist_level = moisture_sensor()
 
     # Display data record to screen
     # Display data written by Professor Eddy, but modified by Nick
     timestamp = now.strftime("%m/%d/%Y %H:%M")
     outstring = str(timestamp) + "\t" + str(format(highTemp, '10.4f')) + "F" \
     + str(format(lowTemp, '10.4f')) + "F" + str(format(averageTemp, '10.4f')) + "F" \
-    + str(format(moist_level, '10.0f')) + "\n"
+    + str(format(moist_level, '10.2f')) + '%' "\n"
     print(outstring.rstrip())
 
     # Return the Average temp
